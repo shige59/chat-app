d064771ca3ca600ad377415d16cf7f5bdc4d3edc
shige59 <141386004+shige59@users.noreply.github.com>
2023-08-23 16:54:16 +0900

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
+  has_many :messages
   # テーブル名は、room_user
   # でも、has-manyの後に来るからroom_users
   validates :name, presence: true
