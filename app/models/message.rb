a598aafb92c9bbfae0f72277e8e86d3f941469ba
shige59 <141386004+shige59@users.noreply.github.com>
2023-08-27 16:08:37 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+
+  has_one_attached :image
+  # カラム追加しなくていいが、imageカラムができるイメージ。
   
   validates :content, presence: true
 end
